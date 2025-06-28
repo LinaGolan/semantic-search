a31f3408a85d923a2c9ec7877fead81da217c5f7
LinaGolan <107746399+LinaGolan@users.noreply.github.com>
2025-06-28 23:34:19 +0300

    fill my solution in diet_classifiers.py

--- nb/src/diet_classifiers.py
+++ nb/src/diet_classifiers.py
@@ -4,6 +4,12 @@ from argparse import ArgumentParser
 from typing import List
 from time import time
 import pandas as pd
+import ast
+import regex as re
+# !pip install sentence-transformers
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
 try:
     from sklearn.metrics import classification_report
 except ImportError:
@@ -11,23 +17,52 @@ except ImportError:
     def classification_report(y, y_pred):
         print("sklearn is not installed, skipping classification report")
 
+model = SentenceTransformer('all-MiniLM-L6-v2')
+embedding_ingredients_df = pd.read_parquet("data/embed_ingredients_df.parquet")
 
+def get_most_similar(ingredient, df):
+    # Embed the ingredient
+    query_vec = model.encode([ingredient.lower()], convert_to_numpy=True)[0]
+    # Compare to each USDA food
+    all_vecs = np.vstack(df["embedding"].values)
+    # Normalize vectors
+    query_norm = query_vec / np.linalg.norm(query_vec)
+    all_norms = all_vecs / np.linalg.norm(all_vecs, axis=1, keepdims=True)
+    # Compute cosine similarities (dot products of normalized vectors)
+    cosine_similarities = all_norms @ query_norm
+    # Get index of max cosine similarity (best match)
+    top_idx = np.argmax(cosine_similarities)
+    closest_match = df.iloc[top_idx]
+    similarity_score = cosine_similarities[top_idx]
+    return closest_match, similarity_score
+
+    
 def is_ingredient_keto(ingredient: str) -> bool:
-    # TODO: Implement
-    return False
+    match, dist = get_most_similar(ingredient, embedding_ingredients_df)
+    # print(f"for ingredient: {ingredient}, match: {match['ingredient_name']}")
+    if match["is_keto"]:
+        return True # Low-carb
+    else:
+        return False # High-carb
 
 
 def is_ingredient_vegan(ingredient: str) -> bool:
-    # TODO: Implement
-    return False
+    match, dist = get_most_similar(ingredient, embedding_ingredients_df)
+    # print(f"for ingredient: {ingredient}, match: {match['ingredient_name']}")
+    if match["is_vegan"]:
+        return True # Vegan
+    else:
+        return False # non Vegan
 
 
 def is_keto(ingredients: List[str]) -> bool:
-    return all(map(is_ingredient_keto, ingredients))
+    list_ingredients = re.findall(r"'([^']*)'", ingredients)
+    return all(map(is_ingredient_keto, list_ingredients))
 
 
 def is_vegan(ingredients: List[str]) -> bool:
-    return all(map(is_ingredient_vegan, ingredients))
+    list_ingredients = re.findall(r"'([^']*)'", ingredients)
+    return all(map(is_ingredient_vegan, list_ingredients))
 
 
 def main(args):
